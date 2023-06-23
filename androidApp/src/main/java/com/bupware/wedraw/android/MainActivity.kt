adeff86c644ab8394fa46ad58013a94fcbe4775a
ricardogdz <ricardogdz6@gmail.com>
2023-06-23 18:19:30 +0200

    Soy la primera bala

--- androidApp/src/main/java/com/bupware/wedraw/android/MainActivity.kt
+++ androidApp/src/main/java/com/bupware/wedraw/android/MainActivity.kt
@@ -35,6 +35,6 @@ fun GreetingView(text: String) {
 @Composable
 fun DefaultPreview() {
     MyApplicationTheme {
-        GreetingView("Hello, Android!")
+        GreetingView("Hello, ndroid!")
     }
 }
